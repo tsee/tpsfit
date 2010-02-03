6809b739e1c9f055adcf1b1c573892bcf5c3cf53
Steffen Mueller <smueller@cpan.org>
2010-02-03 19:19:28 +0100

    more useful testing functions/macros

--- test/cmp.h
+++ test/cmp.h
@@ -1,10 +1,21 @@
 
 #include "linalg3d.h"
+#include <iostream>
+
+#define ASSERT(cond, desc) if (!(cond)) \
+    std::cout << "not "; \
+  std::cout << "ok " << ++testNo << " - " << desc << std::endl;
+
 
 namespace TPS {
   bool equal(double a, double b) {
     return a+kEPSILON > b && b+kEPSILON > a;
   }
-  #undef EPSILON
+}
+
+void assertStream(std::istream& str, unsigned int& testNo) {
+  if (!str.good())
+    std::cout << "not ";
+  std::cout << "ok " << ++testNo << " - input stream still good" << std::endl;
 }
 
