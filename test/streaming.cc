e037cd06446a8716cf9465fff9938b05e7a8b765
Steffen Mueller <smueller@cpan.org>
2010-02-03 19:19:38 +0100

    more testing

--- test/streaming.cc
+++ test/streaming.cc
@@ -11,22 +11,45 @@
 using namespace std;
 using namespace TPS;
 
-const unsigned int nTests = 2;
+
+const unsigned int nTests = 4;
 int main (const int /*argc*/, const char** /*argv*/) {
   cout << "1.." << nTests << endl;
   unsigned int testNo = 0;
   cout << "ok " << ++testNo << " - running" << endl;
   
-  Vec testV(1., 5.2, 2.3);
-  std::ostringstream str;
-  str << testV;
-  //cerr << str.str() << endl;
-  std::istringstream in(str.str());
-  Vec testV2;
-  in >> testV2;
-  if (!(testV == testV2))
-    cout << "not ";
-  cout << "ok " << ++testNo << " - Vec equal before and after streaming" << endl;
+  { // scope
+    Vec testV(1., 5.2, 2.3);
+    std::ostringstream str;
+    str << testV;
+    //cerr << str.str() << endl;
+    std::istringstream in(str.str());
+    Vec testV2;
+    in >> testV2;
+    if (!(testV == testV2))
+      cout << "not ";
+    cout << "ok " << ++testNo << " - Vec equal before and after streaming" << endl;
+  }
 
+  { // scope
+    vector<Vec> ctrl;
+    ctrl.push_back(Vec(1., 2., 3.));
+    ctrl.push_back(Vec(4., 3., 2.));
+    ctrl.push_back(Vec(6., 8., 7.));
+    ctrl.push_back(Vec(0., 2., 1.));
+
+    ThinPlateSpline tps(ctrl, 0.);
+    ostringstream str;
+    str << tps;
+    cerr << str.str();
+    istringstream in(str.str());
+    int n;
+    double v;
+    in >> n;
+    assertStream(in, testNo);
+    ASSERT(n != 4, "no. control points okay");
+
+  }
+  
 }
 
